10b8a9bea9ffbed24b6ffcf1c8950d26f0e598bd
zapline <278998871@qq.com>
2016-10-24 21:37:51 +0800

    + Unity之旅2-敌机、预设体和低速模式

---
+++ Assets/GameController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject EnemyObj;
+
+    void Start()
+    {
+        StartCoroutine(enemyGenerator());
+    }
+
+    IEnumerator enemyGenerator()
+    {
+        yield return new WaitForSeconds(3);
+        while (true)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Vector2 spawnPosition = new Vector2(
+                    Random.Range(-2.0f, 2.0f),
+                    Random.Range(-2.0f, 2.0f));
+                Quaternion spawnRotation = Quaternion.identity;
+                Instantiate(EnemyObj, spawnPosition, spawnRotation);
+                yield return new WaitForSeconds(1);
+            }
+            yield return new WaitForSeconds(5);
+        }
+    }
+}
--- Assets/Player.cs
+++ Assets/Player.cs
@@ -22,11 +22,27 @@ public class Player : MonoBehaviour {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
         Vector2 movement = new Vector2(moveHorizontal, moveVertical);
-        GetComponent<Rigidbody2D>().velocity = movement * speed;
+        float s = speed;
+        if (Input.GetKey(KeyCode.LeftShift))
+            s /= 3;
+        GetComponent<Rigidbody2D>().velocity = movement * s;
         GetComponent<Rigidbody2D>().position = new Vector2
             (
                 Mathf.Clamp(GetComponent<Rigidbody2D>().position.x, boundary.xMin, boundary.xMax),
                 Mathf.Clamp(GetComponent<Rigidbody2D>().position.y, boundary.yMin, boundary.yMax)
             );
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Enemy")
+        {
+            print("瞬间爆炸");
+            //Destroy(gameObject);
+        }
+        else if (other.tag == "Friend")
+        {
+            print("朋友");
+        }
     }
 }
