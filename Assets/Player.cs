b9a076a79f474c63cf1672af439d7406fd5aa126
zapline <278998871@qq.com>
2016-10-24 16:53:22 +0800

    + Unity之旅1-可以移动的飞机

---
+++ Assets/Player.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Boundary
+{
+    public float xMin, xMax, yMin, yMax;
+}
+
+public class Player : MonoBehaviour {
+    public float speed = 10;
+    public Boundary boundary;
+
+    void Start () {
+    }
+
+    void Update () {
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 movement = new Vector2(moveHorizontal, moveVertical);
+        GetComponent<Rigidbody2D>().velocity = movement * speed;
+        GetComponent<Rigidbody2D>().position = new Vector2
+            (
+                Mathf.Clamp(GetComponent<Rigidbody2D>().position.x, boundary.xMin, boundary.xMax),
+                Mathf.Clamp(GetComponent<Rigidbody2D>().position.y, boundary.yMin, boundary.yMax)
+            );
+    }
+}
